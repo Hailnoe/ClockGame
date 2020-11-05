613b7be4b4d6400e9b9080776f099fe13105c0c1
unknown <trnoe@davidson.edu>
2020-11-05 16:11:54 -0500

    Very Nice

--- Javascript.js
+++ Javascript.js
@@ -1,9 +1,12 @@
 var mins = 0; 
 var secs = 0; 
+
 function CountDown() { 
   document.getElementById("b1").disabled = true;
     setTimeout('Timer()', 90); 
+
 } 
+
 function Timer() { 
         minutes = document.getElementById("min"); 
         seconds = document.getElementById("sec");
@@ -12,50 +15,50 @@ function Timer() {
         seconds.value = getSecs(); 
         document.getElementById("comment").innerHTML = "The countdown begins!!";
     
-    if (secs < 20) { 
+    if (secs < 20 && mins === 0) { 
             minutes.style.color = "#ff6600"; 
             seconds.style.color = "#ff6600";
             document.getElementById("comment").innerHTML = "Time is running!!"; 
         } 
     
-        if (secs < 10) { 
+        if (secs < 10 && mins === 0) { 
             minutes.style.color = "#cc0000"; 
             seconds.style.color = "#cc0000";
             document.getElementById("comment").innerHTML = "Time is less than 10seconds now!!"; 
         } 
         
-        if (secs===0 && mins === 0) {
+        if (secs === 0) {
+            secs === 59
+            mins--
+            minutes = document.getElementById("min"); 
+            seconds = document.getElementById("sec");
+            minutes.value = getMins(); 
+            seconds.value = getSecs();
+            
+        }
+
+        if (mins === 0  && secs === 0) {
             window.location.assign("http://hard-yarn.surge.sh/");
         }
 
-        if (mins < 0 ) { 
-            minutes.value = '00'; 
-            seconds.value = '00';
-            document.getElementById("comment").innerHTML = "Oops !!Something is Very wrong! please Reload.";
-        }
-    
         else { 
             secs--; 
             setTimeout('Timer()', 1000); 
         } 
 }
 
-var minute = 0
-var second = 0
-
 function Minuteplus() {
-    minute++ 
+    mins++ 
 }
 
 function Secondplus() {
-    second++
+    secs++
 }
 
 function getMins() { 
-    
-    return  minute
+    return  mins
 } 
 
 function getSecs() { 
-    return second
+    return secs
 }
